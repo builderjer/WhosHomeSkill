e2c096798d7b4db51d9b3541d9c7f6c904c9d8bd
builderjer <builderjer@gmail.com>
2019-01-07 21:35:48 -0700

    bug fix

--- __init__.py
+++ __init__.py
@@ -14,9 +14,26 @@ class WhosHome(MycroftSkill):
         
     @intent_handler(IntentBuilder("WhosHomeIntent").require("WhosKeyword").require("HomeKeyword"))
     def handle_whos_home_intent(self, message):
-        self.speak_dialog("I am home")
+        if ping("192.168.0.20"):
+            self.speak_dialog("Jeremy is home")
+        else:
+            self.speak_dialog("Nobody")
+        #self.speak_dialog("I am home")
         
-        
+def ping(host):
+    """
+    Returns True if host responds to a ping request
+    """
+    import subprocess, platform
+
+    # Ping parameters as function of OS
+    ping_str = "-n 1" if  platform.system().lower()=="windows" else "-c 1"
+    args = "ping " + " " + ping_str + " " + host
+    need_sh = False if  platform.system().lower()=="windows" else True
+
+    # Ping
+    return subprocess.call(args, shell=need_sh) == 0
+
 def create_skill():
     return WhosHome()
         
