7f657d87a5a1fcbd086d2309d8d53fcc49bc6c55
builderjer <builderjer@gmail.com>
2019-01-07 21:16:39 -0700

    first commit

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+from mycroft import intent_handler
+
+__author__ = 'builderjer'
+
+LOGGER = getLogger(__name__)
+
+class WhosHome(MycroftSkill):
+    def __init__(self):
+        super(WhosHome, self).__init__(name="WhosHome")
+        self.people = []
+        
+    @intent_handler(IntentBuilder("WhosHomeIntent").require("WhosKeyword").require("HomeKeyword")
+    def handle_whos_home_intent(self, message):
+        self.speak_dialog("I am home")
+        
+        
